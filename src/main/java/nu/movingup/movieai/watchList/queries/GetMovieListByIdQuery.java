1984e785d534914a2bb1a0d3cbcb381055d75c03
bram <nogrend@msn.com>
2024-02-13 14:45:30 +0100

    get movie list by id

---
+++ src/main/java/nu/movingup/movieai/watchList/queries/GetMovieListByIdQuery.java
@@ -0,0 +1,7 @@
+package nu.movingup.movieai.watchList.queries;
+
+import java.util.UUID;
+
+// todo why do we call it "...MovieList..." instead of "...WatchList..."?
+public record GetMovieListByIdQuery(UUID id) {
+}
